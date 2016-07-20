b57f948270a908362326723db2ef0de2dfe87239
hotjoyit <hotjoyit@gmial.com>
2016-07-20 17:37:49 +0900

    UserDao UnitTest 강화
    * user count 기능 추가
    * 존재하지 않는 user 조회에 대한 Exception 추가

--- src/main/me/hotjoyit/user/dao/UserDao.java
+++ src/main/me/hotjoyit/user/dao/UserDao.java
@@ -36,17 +36,22 @@ public class UserDao {
     PreparedStatement ps = c.prepareStatement(
         "select * from users where id = ?");
     ps.setString(1, id);
-
     ResultSet rs = ps.executeQuery();
-    rs.next();
-    User user = new User();
-    user.setId(rs.getString("id"));
-    user.setName(rs.getString("name"));
-    user.setPassword(rs.getString("password"));
 
+    User user = null;
+    if (rs.next()) {
+      user = new User();
+      user.setId(rs.getString("id"));
+      user.setName(rs.getString("name"));
+      user.setPassword(rs.getString("password"));
+    }
     rs.close();
     ps.close();
     c.close();
+
+    if (user == null) {
+      throw new SQLDataException();
+    }
 
     return user;
   }
--- src/test/me/hotjoyit/user/dao/UserDaoTest.java
+++ src/test/me/hotjoyit/user/dao/UserDaoTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.sql.SQLDataException;
 import java.sql.SQLException;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -18,23 +19,56 @@ public class UserDaoTest {
   @Test
   public void addAndGet() throws SQLException {
     ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+    UserDao dao = context.getBean("userDao", UserDao.class);
+    User user1 = new User("no1", "홍길동", "pw01");
+    User user2 = new User("no2", "임꺽정", "pw02");
 
+    dao.deleteAll();
+    assertThat(dao.getCount(), is(0));
+
+    dao.add(user1);
+    dao.add(user2);
+    assertThat(dao.getCount(), is(2));
+
+    User userget1 = dao.get(user1.getId());
+    assertThat(userget1.getName(), is(user1.getName()));
+    assertThat(userget1.getPassword(), is(user1.getPassword()));
+
+    User userget2 = dao.get(user2.getId());
+    assertThat(userget2.getName(), is(user2.getName()));
+    assertThat(userget2.getPassword(), is(user2.getPassword()));
+  }
+
+  @Test
+  public void count() throws SQLException {
+    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+    UserDao dao = context.getBean("userDao", UserDao.class);
+
+    User user1 = new User("no1", "홍길동", "pw01");
+    User user2 = new User("no2", "임꺽정", "pw02");
+    User user3 = new User("no3", "손오공", "pw03");
+
+    dao.deleteAll();
+    assertThat(dao.getCount(), is(0));
+
+    dao.add(user1);
+    assertThat(dao.getCount(), is(1));
+
+    dao.add(user2);
+    assertThat(dao.getCount(), is(2));
+
+    dao.add(user3);
+    assertThat(dao.getCount(), is(3));
+  }
+
+  @Test(expected = SQLDataException.class)
+  public void getUserFailure()throws SQLException {
+    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
     UserDao dao = context.getBean("userDao", UserDao.class);
 
     dao.deleteAll();
     assertThat(dao.getCount(), is(0));
 
-    User user = new User();
-    user.setId("hotjoyit");
-    user.setName("조우현");
-    user.setPassword("single");
-
-    dao.add(user);
-    assertThat(dao.getCount(), is(1));
-
-    User user2 = dao.get(user.getId());
-
-    assertThat(user2.getName(), is(user.getName()));
-    assertThat(user2.getPassword(), is(user.getPassword()));
+    dao.get("unknown_id");
   }
 }
