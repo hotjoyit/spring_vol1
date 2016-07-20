52a6d74fe8691e42e55fe3b6463c84d348bf78b7
hotjoyit <hotjoyit@gmial.com>
2016-07-21 07:52:32 +0900

    UserDao클래스에 존재하던 JDBC Context 코드를 JdbcContext 클래스로 추출 : 단일 책임의 원칙

--- src/main/me/hotjoyit/user/dao/UserDao.java
+++ src/main/me/hotjoyit/user/dao/UserDao.java
@@ -10,13 +10,18 @@ import java.sql.*;
  */
 public class UserDao {
   private DataSource dataSource;
+  private JdbcContext jdbcContext;
 
   public void setDataSource(DataSource dataSource) {
     this.dataSource = dataSource;
   }
 
+  public void setJdbcContext(JdbcContext jdbcContext) {
+    this.jdbcContext = jdbcContext;
+  }
+
   public void add(final User user) throws SQLException {
-    jdbcContextWithStatementStrategy(new StatementStrategy() {
+    jdbcContext.workWithStatementStrategy(new StatementStrategy() {
       @Override
       public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
@@ -56,7 +61,7 @@ public class UserDao {
   }
 
   public void deleteAll() throws SQLException {
-    jdbcContextWithStatementStrategy(new StatementStrategy() {
+    jdbcContext.workWithStatementStrategy(new StatementStrategy() {
       @Override
       public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
         PreparedStatement ps = c.prepareStatement("delete from users");
@@ -80,31 +85,5 @@ public class UserDao {
     return count;
   }
 
-  public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-    Connection c = null;
-    PreparedStatement ps = null;
 
-    try {
-      c = dataSource.getConnection();
-      ps = stmt.makePreparedStatement(c);
-      ps.executeUpdate();
-    } catch (SQLException e) {
-      throw e;
-    } finally {
-      if (ps != null) {
-        try {
-          ps.close();
-        } catch (SQLException e) {
-
-        }
-      }
-      if (c != null) {
-        try {
-          c.close();
-        } catch (SQLException e) {
-
-        }
-      }
-    }
-  }
 }
