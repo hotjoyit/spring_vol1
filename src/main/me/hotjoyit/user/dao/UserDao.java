479682719f151f2d6d746f20850ff40267a3fc90
hotjoyit <hotjoyit@gmial.com>
2016-07-21 16:44:34 +0900

    UserDao 인터페이스 추출

--- src/main/me/hotjoyit/user/dao/UserDao.java
+++ src/main/me/hotjoyit/user/dao/UserDao.java
@@ -1,53 +1,21 @@
 package me.hotjoyit.user.dao;
 
 import me.hotjoyit.user.domain.User;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 
-import javax.sql.DataSource;
-import java.sql.*;
 import java.util.List;
 
 /**
  * Created by hotjoyit on 2016-07-19
  */
-public class UserDao {
-  private JdbcTemplate jdbcTemplate;
+public interface UserDao {
 
-  private RowMapper<User> userMapper = new RowMapper<User>() {
-    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-      User user = new User();
-      user.setId(rs.getString("id"));
-      user.setName(rs.getString("name"));
-      user.setPassword(rs.getString("password"));
-      return user;
-    }
-  };
+  void add(final User user);
 
-  public void setDataSource(DataSource dataSource) {
-    this.jdbcTemplate = new JdbcTemplate(dataSource);
-  }
+  User get(String id);
 
-  public void add(final User user) {
-    jdbcTemplate.update(
-        "insert into users(id, name, password) values(?,?,?)"
-        , user.getId(), user.getName(), user.getPassword());
-  }
+  void deleteAll();
 
-  public User get(String id) {
-    return jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id}, this.userMapper);
-  }
+  int getCount();
 
-  public void deleteAll() {
-    jdbcTemplate.update("delete from users");
-  }
-
-
-  public int getCount() {
-    return jdbcTemplate.queryForInt("select count(*) from users");
-  }
-
-  public List<User> getAll() {
-    return jdbcTemplate.query("select * from users order by id", userMapper);
-  }
+  List<User> getAll();
 }
---
+++ src/main/me/hotjoyit/user/dao/UserDaoJdbc.java
@@ -0,0 +1,54 @@
+package me.hotjoyit.user.dao;
+
+import me.hotjoyit.user.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by hotjoyit on 2016-07-21
+ */
+public class UserDaoJdbc implements UserDao {
+  private JdbcTemplate jdbcTemplate;
+
+  private RowMapper<User> userMapper = new RowMapper<User>() {
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+      User user = new User();
+      user.setId(rs.getString("id"));
+      user.setName(rs.getString("name"));
+      user.setPassword(rs.getString("password"));
+      return user;
+    }
+  };
+
+  public void setDataSource(DataSource dataSource) {
+    this.jdbcTemplate = new JdbcTemplate(dataSource);
+  }
+
+  public void add(final User user) {
+    jdbcTemplate.update(
+        "insert into users(id, name, password) values(?,?,?)"
+        , user.getId(), user.getName(), user.getPassword());
+  }
+
+  public User get(String id) {
+    return jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id}, this.userMapper);
+  }
+
+  public void deleteAll() {
+    jdbcTemplate.update("delete from users");
+  }
+
+
+  public int getCount() {
+    return jdbcTemplate.queryForInt("select count(*) from users");
+  }
+
+  public List<User> getAll() {
+    return jdbcTemplate.query("select * from users order by id", userMapper);
+  }
+}
