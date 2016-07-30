54b152d4a7093d490b78bef0de78183e04c7274d
hotjoyit <hotjoyit@gmial.com>
2016-07-30 15:19:04 +0900

    Bean 설정에 존재하던 SQL을 별도의 설정으로 분리함

---
+++ src/main/me/hotjoyit/user/sqlservice/XmlSqlService.java
@@ -0,0 +1,44 @@
+package me.hotjoyit.user.sqlservice;
+
+import me.hotjoyit.user.dao.UserDao;
+import me.hotjoyit.user.sqlservice.jaxb.SqlType;
+import me.hotjoyit.user.sqlservice.jaxb.Sqlmap;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by hotjoyit on 2016-07-30
+ */
+public class XmlSqlService implements SqlService {
+  private Map<String, String> sqlMap = new HashMap<>();
+
+  public XmlSqlService() {
+    String contextPath = Sqlmap.class.getPackage().getName();
+    try {
+      JAXBContext context = JAXBContext.newInstance(contextPath);
+      Unmarshaller unmarshaller = context.createUnmarshaller();
+      InputStream is = UserDao.class.getResourceAsStream("sqlmap.xml");
+      Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(is);
+
+      for (SqlType sql : sqlmap.getSql()) {
+        sqlMap.put(sql.getKey(), sql.getValue());
+      }
+    } catch (JAXBException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public String getSql(String key) throws SqlRetrievalFailureException {
+    String sql = sqlMap.get(key);
+    if (sql == null) {
+      throw new SqlRetrievalFailureException(key + "를 이용해서 SQL을 찾을 수 없습니다");
+    }
+    return sql;
+  }
+}
